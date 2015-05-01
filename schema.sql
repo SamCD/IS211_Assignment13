f8ac554fa3634705dec5dbeca9500422cf9ffa10
SamCD <samuel.cohen-devries@spsmail.cuny.edu>
2015-05-01 10:09:14 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,13 +1,20 @@
-CREATE TABLE Students (ID INT PRIMARY KEY,
+DROP TABLE IF EXISTS Students;
+DROP TABLE IF EXISTS Quizzes;
+DROP TABLE IF EXISTS Results;
+CREATE TABLE Students (ID INTEGER PRIMARY KEY,
 						firstName TEXT,
-						lastName TEXT)
+						lastName TEXT);
 						
-CREATE TABLE Quizzes (ID INT PRIMARY KEY,
+CREATE TABLE Quizzes (ID INTEGER PRIMARY KEY,
 						subject TEXT,
 						questions INT,
-						testDate date)
+						testDate date);
 						
-CREATE TABLE Results (ID INT PRIMARY KEY,
+CREATE TABLE Results (ID INTEGER PRIMARY KEY,
 						quizID INT,
 						studentID INT,
-						grade float)
+						grade float);
+
+INSERT INTO Students VALUES(NULL,'John','Smith');
+INSERT INTO Quizzes VALUES(NULL,'Python Basics',5,'02/05/2015');
+INSERT INTO Results select NULL,(select ID from Quizzes where subject = 'Python Basics'),(select ID from Students where firstName = 'John' and lastName = 'Smith'),85;
