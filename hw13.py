46d9c38e8733df350566e352b9a292e8cb5b496a
SamCD <samuel.cohen-devries@spsmail.cuny.edu>
2015-05-05 09:56:14 -0400

    Update hw13.py

--- hw13.py
+++ hw13.py
@@ -1,7 +1,8 @@
-import sqlite3
+import sqlite3,json
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from contextlib import closing
+
 
 DATABASE = 'hw13.db'
 DEBUG = True
@@ -21,6 +22,7 @@ def init_db():
             db.cursor().executescript(f.read())
         db.commit()
 
+connect_db()
 init_db()
 
 @app.before_request
@@ -60,38 +62,72 @@ def logout():
 @app.route('/dashboard')
 def show_tests():
     cur1 = g.db.execute('select ID, firstName, lastName from Students')
-    students = {row[0]:(row[2],row[1]) for row in cur1.fetchall()}
+    students = [dict(firstName=row[1],lastName=row[2]) for row in cur1.fetchall()]
     cur2 = g.db.execute('select ID, subject, questions, testDate from Quizzes')
-    quizzes = {row[0]:(row[1],row[2],row[3]) for row in cur2.fetchall()}
-    print students
-    print quizzes
+    quizzes = [dict(subject=row[1],questions=row[2],testDate=row[3])
+                    for row in cur2.fetchall()]
     return render_template('show_tests.html'
                            , students=students
                            , quizzes=quizzes)
 
-@app.route('/student/add', methods=['POST'])
+@app.route('/student/add', methods=['GET','POST'])
 def add_student():
-    if not session.get('logged_in'):
-        abort(401)
-    g.db.execute('insert into Students (firstName, lastName) values (?, ?)',
-                 [request.form['firstName'], request.form['lastName']])
-    g.db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_tests'))
+    if request.method == 'POST':
+        if not session.get('logged_in'):
+            abort(401)
+        g.db.execute('insert into Students (firstName, lastName) values (?, ?)',
+                     [request.form['firstName'], request.form['lastName']])
+        g.db.commit()
+        flash('New student successfully posted')
+        return redirect(url_for('show_tests'))
+    return render_template('add_student.html')
 
-@app.route('/quiz/add', methods=['POST'])
+@app.route('/student/<id>')
+def student_results(id):
+    msg = None
+    cur = g.db.execute('select quiz, grade from Results where student=?',
+                       (id,))
+    results = [dict(quiz=row[0],grade=row[1]) for row in cur.fetchall()]
+    if not results:
+        msg = 'No results'
+    return render_template('student_results.html',results=results,msg=msg)
+
+@app.route('/quiz/add', methods=['GET','POST'])
 def add_quiz():
-    if not session.get('logged_in'):
-        abort(401)
-    g.db.execute('insert into Quizzes (subject'\
+    if request.method == 'POST':
+        if not session.get('logged_in'):
+            abort(401)
+        g.db.execute('insert into Quizzes (subject'\
                                      ', questions, testDate) '\
                                      'values (?, ?, ?)',
-                 [request.form['subject']
-                  , request.form['questions']
-                  , request.form['testDate']])
-    g.db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_tests'))
+                     [request.form['subject']
+                      , request.form['questions']
+                      , request.form['testDate']])
+        g.db.commit()
+        flash('New quiz successfully posted')
+        return redirect(url_for('show_tests'))
+    return render_template('add_quiz.html')
 
+@app.route('/results/add', methods=['GET','POST'])
+def add_result():
+    cur1 = g.db.execute('select ID from Students')
+    students = [dict(ID=row[0]) for row in cur1.fetchall()]
+    cur2 = g.db.execute('select ID from Quizzes')
+    quizzes = [dict(ID=row[0]) for row in cur2.fetchall()]
+    if request.method == 'POST':
+        if not session.get('logged_in'):
+            abort(401)
+        g.db.execute(
+    'insert into Results (quiz,student,grade) values (?,?,?)',
+    (
+        request.form.get('Quiz'),
+        request.form.get('Student'),
+        request.form.get('grade')
+    )
+    )
+        g.db.commit()
+        flash('New result successfully posted')
+        return redirect(url_for('show_tests'))
+    return render_template('add_result.html',students=students,quizzes=quizzes)
 if __name__ == '__main__':
     app.run()
